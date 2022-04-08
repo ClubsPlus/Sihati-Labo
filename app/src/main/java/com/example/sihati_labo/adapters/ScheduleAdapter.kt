b32901f4b672183188b810bb9c0d2d2186cd9a63
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-04-08 08:13:58 +0100

    add the onclick listener

--- app/src/main/java/com/example/sihati_labo/adapters/ScheduleAdapter.kt
+++ app/src/main/java/com/example/sihati_labo/adapters/ScheduleAdapter.kt
@@ -2,14 +2,14 @@ package com.example.sihati_labo.adapters
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.paris.extensions.style
 import com.example.sihati_labo.Database.Schedule
 import com.example.sihati_labo.R
 
@@ -32,7 +32,7 @@ class ScheduleAdapter(
         return ScheduleViewHolder(itemView)
     }
 
-    @SuppressLint("ResourceType")
+    @SuppressLint("ResourceType", "SetTextI18n")
     override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
         // on below line we are setting data to item of recycler view.
         holder.date.text = allSchedules[position].date
@@ -41,19 +41,23 @@ class ScheduleAdapter(
         holder.person.text = allSchedules[position].person.toString()+"/"+allSchedules[position].limite.toString()
         val progress = ((allSchedules[position].person)!! *100)/ allSchedules[position].limite!!
         holder.progressBar.progress = progress
-        val progressDrawable: Drawable = holder.progressBar.progressDrawable.mutate()
 
-        when (progress) {
-            in 0..45 ->progressDrawable.setColorFilter(R.color.green, PorterDuff.Mode.SRC_IN)
-            in 46..80 ->progressDrawable.setColorFilter(R.color.yellow, PorterDuff.Mode.SRC_IN)
-            else ->progressDrawable.setColorFilter(R.color.red, PorterDuff.Mode.SRC_IN)
-        }
-        holder.progressBar.progressDrawable = progressDrawable
+        //change the color of the progressbar
+//        holder.progressBar.style(R.style.progressBar)
+//        when (progress) {
+//            in 0..45 -> progressDrawable.setTint(R.color.green)
+//            in 46..80 -> progressDrawable.setTint(R.color.yellow)
+//            else -> progressDrawable.setTint(R.color.red)
+//        }
+//        holder.progressBar.progressDrawable = progressDrawable
+
+        holder.schedule.setOnClickListener { taskClickInterface?.onClick(allSchedules[position]) }
     }
 
     override fun getItemCount() = allSchedules.size
 
     // below method is use to update our list of notes.
+    @SuppressLint("NotifyDataSetChanged")
     fun updateList(newList: List<Schedule>) {
         // on below line we are clearing
         // our notes array list
@@ -70,16 +74,17 @@ class ScheduleAdapter(
         RecyclerView.ViewHolder(itView){
         // on below line we are creating an initializing all our
         // variables which we have added in layout file.
-        val date = itemView.findViewById<TextView>(R.id.date)
-        val startTime = itemView.findViewById<TextView>(R.id.startTime)
-        val endTime = itemView.findViewById<TextView>(R.id.endTime)
-        val person = itemView.findViewById<TextView>(R.id.persons)
-        val progressBar = itemView.findViewById<ProgressBar>(R.id.progressBar)
+        val date: TextView = itemView.findViewById(R.id.date)
+        val startTime: TextView = itemView.findViewById(R.id.startTime)
+        val endTime: TextView = itemView.findViewById(R.id.endTime)
+        val person: TextView = itemView.findViewById(R.id.persons)
+        val progressBar: ProgressBar = itemView.findViewById(R.id.progressBar)
+        val schedule: ConstraintLayout = itemView.findViewById(R.id.schedule)
     }
 
     interface TaskClickInterface {
         // creating a method for click action
         // on recycler view item for updating it.
-        fun onClick(task: Schedule)
+        fun onClick(schedule: Schedule)
     }
 }
